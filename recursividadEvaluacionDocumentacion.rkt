a6f01cfa4fbd06eb85504ada07e69483d38bda9e
Jean Carlos González Valverde <jean.gonzalez.v@usach.cl>
2022-04-03 17:54:46 -0400

    Create recursividadEvaluacionDocumentacion.rkt

---
+++ recursividadEvaluacionDocumentacion.rkt
@@ -0,0 +1,21 @@
+; 1) La función H esta definida por la siguiente regla:
+
+; H(n) = n si n<3 
+
+; H(n) = H(n - 1) + 2H(n - 2) + 3H(n - 3) si n>=3.
+
+; Implementar la función en Racket/Scheme. 
+
+
+
+; 2) Implementar la función anterior a partir de recursión de cola.
+
+
+
+; 3) Discuta y responda la siguientes preguntas
+
+; a. ¿Porqué usaría recursión de cola y no recursión natural?. 
+
+; b. ¿Ve algún beneficio y/o deventaja entre usar recursión de cola, arborea y natural?. ¿en que casos usaría una u otra?
+
+; c. ¿Cómo podría migrar una función recursiva natural a recursión de cola?"

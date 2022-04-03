58150aef01685ac97c402c9680dd6d65c4c7f8d7
Jean Carlos González Valverde <jean.gonzalez.v@usach.cl>
2022-04-03 17:53:11 -0400

    Create lambdaFuncionesCurrificacion.rkt

---
+++ lambdaFuncionesCurrificacion.rkt
@@ -0,0 +1,19 @@
+;1) De acuerdo al cálculo lambda exprese una función que permita calcular el área de un cuadrado, rectángulo y circulo.
+
+
+;2) A continuación, implemente estas funciones directamente en el lenguaje Scheme/Racket a partir de lo indicado en el cálculo lambda
+
+
+;3) De acuerdo al cálculo lambda exprese una función ánonima que permita calcular el valor absoluto de un número y luego ilustre como usar esta función para calcular el valor absoluto de -5.
+
+
+
+;La composición de funciones f(x), g(x) entrega como resultado la función h(x) correspondiente a f(g(x)). A partir de este concepto,  
+
+
+
+;4) Definir una función que permita componer 3 funciones de manera no currificada
+
+
+;5) Definir una función que permita componer 3 funciones de manera currificada
+

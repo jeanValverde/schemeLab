2c9542264d2623834a6923a3e833e9f87d78ee89
Jean Carlos González Valverde <jean.gonzalez.v@usach.cl>
2022-04-03 17:55:43 -0400

    Create ParesListas.rkt

---
+++ ParesListas.rkt
@@ -0,0 +1,13 @@
+
+; Considere una lista de compras. Esta lista se compone de artículos que se desean comprar. 
+; Cada artículo se conforma por "nombre_articulo" y su precio (asuma que es un tipo de dato numérico) 
+
+
+; Implemente directamente en el lenguaje Scheme/Racket el TDA de la lista de compras que permita:
+
+
+; a. Agregar un artículo a la lista de compras. Por ejemplo: (add-item miListaCompra "nombre_articulo" 500)
+; b. Remover un artículo de la lista de compras. Por ejemplo: (remove-item miListaCompra "nombre_articulo")
+; a. Asignar o cambiar el precio de un artículo de la lista de compras. Por ejemplo: (set-item-price  miListaCompra"nombre_articulo_ingresado" 100)
+; c. Obtener los productos cuyo precio sea menor que P. Por ejemplo: (articulosConCostoMenorA miListaCompra 300)
+; d. Obtener los productos que cumplan con un determinado criterio dado por la función f. Por ejemplo: (articulosQueCumplanCon miListaCompra (lambda (e) (>(precio e) 50) ) )

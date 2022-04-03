2347a09894aaedea981a2f8465635579287bb194
Jean Carlos González Valverde <jean.gonzalez.v@usach.cl>
2022-04-03 17:50:43 -0400

    Create introduccion.rkt

---
+++ introduccion.rkt
@@ -0,0 +1,11 @@
+;1) En el contexto de los lenguajes de programación y sus aplicaciones. ¿Cuáles son los pro y contra de los lenguajes interpretados vs compilados?. 
+; ¿En que escenario escogerían un lenguaje interpretado versus uno compilado?
+
+
+
+;2) Siendo la unidad de abstracción la función y entendiendo que bajo este paradigma todo es considerado función, 
+; ¿cómo se pueden enfrentar problemas fuera del ámbito matemático donde incluso se opera con objetos del mundo real?
+
+
+
+;3) ¿Cuáles son los pro y contra que ve del paradigma funcional?. ¿En que escenario escogerían un enfoque imperativo versus uno funcional?
